b596e0aa841140ba016a47faa23010e4ffe90715
Erick Winck <erickwinck5@gmail.com>
2025-04-12 15:45:44 -0300

    Atualizar rotas de usuário

--- backend/src/routes/usuarios_routes.js
+++ backend/src/routes/usuarios_routes.js
@@ -2,13 +2,17 @@ const express = require("express");
 const router = express.Router();
 const usuariosController = require("../controllers/usuarios_controller");
 
-router.get('/', usuariosController.getUsuarios);
-router.post('/', usuariosController.postUsuario);
-
-router.get('/:id', usuariosController.getUsuarioById);
-
+router.get('/me', usuariosController.getValidateMe);
 router.post('/login', usuariosController.postLogin);
 
-router.get('/me', usuariosController.getValidateMe);
+router.get('/:id/professor', usuariosController.getProfessorByUsuarioId);
+router.get('/:id/aluno', usuariosController.getAlunoByUsuarioId);
+
+router.get('/:id', usuariosController.getUsuarioById);
+router.put('/:id', usuariosController.putUsuario);
+router.delete('/:id', usuariosController.deleteUsuario);
+
+router.get('/', usuariosController.getUsuarios);
+router.post('/', usuariosController.postUsuario);
 
 module.exports = router;
