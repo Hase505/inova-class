7ca2093698bf148c8e78be67c715b0a5c3840274
Erick Winck <erickwinck5@gmail.com>
2025-04-12 15:35:24 -0300

    Corrigir ordem das rotas de disciplinas

--- backend/src/routes/disciplinasRoutes.js
+++ backend/src/routes/disciplinasRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const disciplinasController = require('../controllers/disciplinasController.js');
 
-router.get('/:id', disciplinasController.getDisciplinaById);
 router.get('/:id/professores', disciplinasController.getProfessoresByDisciplinaId);
 router.get('/:id/alunos', disciplinasController.getAlunosByDisciplinaId);
+
+router.get('/:id', disciplinasController.getDisciplinaById);
 router.put('/:id', disciplinasController.putDisciplina);
 router.delete('/:id', disciplinasController.deleteDisciplina);
 
